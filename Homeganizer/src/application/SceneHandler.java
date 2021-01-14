3285f24525b40f97cfede205506cb470c6ca9db2
scatterman99 <db.brunetti4@gmail.com>
2021-01-14 19:09:10 +0100

    Aggiunto SceneHandler (funzioni base)

--- Homeganizer/src/application/SceneHandler.java
+++ Homeganizer/src/application/SceneHandler.java
@@ -1,5 +1,50 @@
 package application;
 
-public class SceneHandler {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class SceneHandler {
+	private Stage stage;
+	private Scene scene;
+
+	private static SceneHandler instance = null;
+
+	public static SceneHandler getInstance() {
+		if (instance == null)
+			instance = new SceneHandler();
+		return instance;
+	}
+
+	private SceneHandler() {
+	}
+
+	public void init(Stage primaryStage) throws Exception {
+		stage = primaryStage;
+		scene = setInitialScene("Login.fxml", 200, 200);// dimensioni arbitrarie per adesso
+		stage.setScene(scene);
+		stage.setTitle("Homeganizer Login");
+		stage.setResizable(false);
+		stage.show();
+
+	}
+
+	private Scene setInitialScene(String filename, int x, int y) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/" + filename));
+		Parent root = (Parent) loader.load();
+		return new Scene(root, x, y);
+	}
+
+	public void goToScene(String filename, String title, int x, int y) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/" + filename));
+		Parent root = (Parent) loader.load();
+		scene.setRoot(root);
+		stage.hide();
+		stage.setTitle(title);
+		stage.setWidth(x + 6);
+		stage.setHeight(x + 35);
+		stage.setResizable(false);
+		stage.show();
+	}
 }
