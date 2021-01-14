1bd4466dda373a5fd23d6af7c0d0cf9af0a99612
Lorenzo <pirolorenzo@live.it>
2021-01-14 19:13:53 +0100

    committ la prima

--- Homeganizer/src/application/Main.java
+++ Homeganizer/src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,6 +11,15 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("logInterface.fxml"));
+			Parent root =  loader.load();
+			Scene scene = new Scene(root, 650, 456);
+			primaryStage.setScene(scene);
+			primaryStage.setMinHeight(400);
+			primaryStage.setMinWidth(400);
+			primaryStage.setTitle("Homeganizer");
+			primaryStage.show();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
