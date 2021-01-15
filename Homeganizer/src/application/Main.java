8fc368546f62df68db4c8fca6c83116bca887808
scatterman99 <db.brunetti4@gmail.com>
2021-01-15 12:26:49 +0100

    Pulizia codice

--- Homeganizer/src/application/Main.java
+++ Homeganizer/src/application/Main.java
@@ -1,17 +1,11 @@
 package application;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
-
-
-
 	public void start(Stage primaryStage) throws Exception {
 		SceneHandler.getInstance().init(primaryStage);
 
