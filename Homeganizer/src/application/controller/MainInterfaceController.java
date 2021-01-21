d4f32991dfc90f9ee5f07a4b2613c185d92a5d8a
alptur-calc <clbmnl99p14d086z@studenti.unical.it>
2021-01-21 16:21:39 +0100

    Aggiunta visualizzazione elenco stanze

--- Homeganizer/src/application/controller/MainInterfaceController.java
+++ Homeganizer/src/application/controller/MainInterfaceController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.GroupLayout.Alignment;
+
 import application.SceneHandler;
 import application.model.RoomHandler;
 import application.view.MessageView;
@@ -13,10 +15,12 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
@@ -26,7 +30,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class MainInterfaceController implements Initializable {
@@ -68,8 +75,6 @@ public class MainInterfaceController implements Initializable {
     @Override
 	public void initialize(URL location, ResourceBundle resources) {
     	
-    	
-    	
 		Pane p = new Pane();
     	btnAddRoom = new Button();
     	btnAddRoom.setPrefSize(225.0, 35.0);
@@ -77,6 +82,7 @@ public class MainInterfaceController implements Initializable {
     	Image buttonImg =  new Image(getClass().getResourceAsStream("/resources/ButtonAddImage.png"));
     	ImageView imgv= new ImageView(buttonImg);
     	btnAddRoom.setGraphic(imgv);
+    	btnAddRoom.setAlignment(Pos.CENTER);
     	btnAddRoom.setOnMousePressed(handleBtnAddRoom);
     	
     	p.getChildren().add(btnAddRoom);
@@ -147,12 +153,34 @@ public class MainInterfaceController implements Initializable {
 			}
 			
 			RoomHandler.getInstance().aggiungiStanza(nome, larghezza, profondità);
-			}catch(NumberFormatException e2) {
+			addStanzaToList(nome);
+			}
+			catch(NumberFormatException e2) {
 				MessageView.showMessageAlert(AlertType.WARNING, "Dati inseriti non validi", "Per favore, riprovare e inserire i dati correttamente.");
 			}
-			
-		
-
-		}};
+		}
+	};
 	
+	private void addStanzaToList(String nome) {
+		Pane p = new Pane();
+		HBox h = new HBox();
+		h.setAlignment(Pos.CENTER);
+		Image img = new Image(getClass().getResourceAsStream("/resources/StanzaIcon.png"));
+		ImageView imgViewIcona = new ImageView(img); 
+		Label nomeStanza = new Label(nome);
+		nomeStanza.setFont(new Font(14));
+		h.getChildren().add(imgViewIcona);
+		h.getChildren().add(nomeStanza);
+		p.getChildren().add(h);
+		p.setOnMouseClicked(handleStanzaInListClicked);
+    	lstRooms.getItems().add(lstRooms.getItems().size()-1,p);  
+	}
+	
+	private EventHandler<MouseEvent> handleStanzaInListClicked = new EventHandler<MouseEvent>() {
+
+		@Override
+		public void handle(MouseEvent event) {
+			MessageView.showMessageAlert(AlertType.INFORMATION, "Stanza", "Ecco la tua stanza, maledetto!!");
+		}
+	};
 }
