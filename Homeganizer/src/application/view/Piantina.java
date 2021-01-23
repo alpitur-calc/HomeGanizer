79f8c317ea0fb244e0b5813ffe7aa18537bacfdd
Lorenzo <pirolorenzo@live.it>
2021-01-23 17:57:10 +0100

    cambiata piantina

--- Homeganizer/src/application/view/Piantina.java
+++ Homeganizer/src/application/view/Piantina.java
@@ -2,8 +2,10 @@ package application.view;
 
 import application.model.Mobile;
 import application.model.Stanza;
+import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class Piantina {
@@ -11,6 +13,8 @@ public class Piantina {
 	private static Piantina istanza = null;
 	private static GraphicsContext gc;
 	private static final int l = 50;
+	private Canvas c;
+	private Mobile mobileSelezionato;
 
 	public static Piantina getInstance() {
 		if (istanza == null) {
@@ -20,10 +24,20 @@ public class Piantina {
 	}
 
 	private Piantina() {
+		
+		
+		
 	}
 
-	public static void disegna(Canvas c, Stanza s) {
+	public void setCanvas(Canvas c)
+	{
+		this.c = c;
+		c.setOnMouseClicked(spostaMobile);
+	}
+	
+	public void disegna(Stanza s) {
 
+		
 		c.setHeight(s.getProfondità() * Piantina.l);
 		c.setWidth(s.getLarghezza() * Piantina.l);
 
@@ -44,11 +58,26 @@ public class Piantina {
 		}
 	}
 
-	public static void evidenziaMobile(Canvas c, Stanza s, Mobile m) {
-		Piantina.disegna(c, s);
+	public void evidenziaMobile(Stanza s, Mobile m) {
+		disegna(s);
 		gc.setFill(Color.GREEN);
 		gc.fillRect(m.getX() * Piantina.l, m.getY() * Piantina.l, m.getW() * Piantina.l, m.getH() * Piantina.l);
 
 	}
+	
+	public EventHandler<MouseEvent> spostaMobile = new EventHandler<MouseEvent>()
+	{
+
+		@Override
+		public void handle(MouseEvent e) {
+			
+			System.out.println(e.getX() - (e.getX()%Piantina.l) + e.getY() - (e.getY()%Piantina.l));
+			
+		}
+		
+	};
+	
+	
+	
 
 }
